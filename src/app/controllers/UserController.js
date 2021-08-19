be47a3519b2f50d1ebe0d01cd4d8f328783cfbc5
Thomé Lucas <chacalgbi@hotmail.com>
2021-08-19 18:15:59 -0300

    Add Update and Show

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -119,6 +119,85 @@ class UserControler{
             });
         });
     }
+
+    async show(req, res){
+        User.find({_id: req.params.id}, '_id nome email cpf telefone endereco cidade createdAt updatedAt').then((user) => {
+            return res.json({
+                error: false,
+                user: user
+            });
+        }).catch((err) => {
+            return res.status(400).json({
+                error: true,
+                code: 107,
+                message: "Não foi possível recuperar o usuário. :("
+            });
+        });
+    }
+
+    async update(req, res){
+        let schema = yup.object().shape({
+            _id: yup.string().required(),
+            nome: yup.string().min(5),
+            email: yup.string().email().min(13),
+            senha: yup.string().min(5),
+            cpf: yup.string().min(10),
+            telefone: yup.string().min(8),
+            endereco: yup.string().min(10),
+            cidade: yup.string().min(3),
+          });
+
+          if(!(await schema.isValid(req.body))){
+            return res.status(400).json({
+                error: true,
+                code: 108,
+                message: "Error: Dados do formulário inválidos ou incompletos!"
+            });
+          }
+
+          const { _id, email } = req.body;
+          const temUsuario = await User.findOne({_id: _id});
+          if(!temUsuario){
+              return res.status(400).json({
+                  error: true,
+                  code: 109,
+                  message: "Erro: Usuário não encontrado"
+              });
+          }
+
+          //Primeiro verifica se o email é diferente do email atual
+          if(email != temUsuario.email){
+            const jaExiste = await User.findOne({email}); // procura se já tem o email no BD
+            if(jaExiste){
+                return res.status(400).json({
+                    error: true,
+                    code: 110,
+                    message: "Erro: Este email já está cadastrado!"
+                });
+            }
+          }
+
+          var dados = req.body;
+          if(dados.senha){ // Se o usuário enviar a senha, encripta a mesma
+              dados.senha = await bcrypt.hash(dados.senha, 7);
+          }
+
+          await User.updateOne({_id: dados._id}, dados, (err) => {
+              if(err){
+                return res.status(400).json({
+                    error: true,
+                    code: 111,
+                    message: "Erro: Não foi possível editar o usuário"
+                });
+              }
+
+              return res.json({
+                error: false,
+                message: "Usuário Editado com sucesso!"
+              });
+          });
+    }
+
 }
 
 export default new UserControler();
--- src/routes.js
+++ src/routes.js
@@ -6,8 +6,10 @@ import authMiddleware from './app/middlewares/auth';
 const routes = new Router();
 
 routes.get('/users', UserController.index);
+routes.get('/users/:id', UserController.show);
 routes.post('/users', UserController.store);
 routes.post('/login', LoginController.store);
+routes.put('/users', UserController.update);
 routes.delete('/users/:id', authMiddleware, UserController.delete);
 
 export default routes;
